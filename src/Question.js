481fbabc5f1bdfaa0df98bf607398987d78c9511
rupamsutar <rupamsutar281998@gmail.com>
2025-12-13 10:45:50 +0530

    Starting a new quiz

---
+++ src/Question.js
@@ -0,0 +1,9 @@
+const Question = (props) => {
+  return (
+    <div>
+      <p>Question</p>
+    </div>
+  )
+};
+
+export default Question;
--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,12 +1,11 @@
-const StartScreen = ({numQuestions}) => {
+const StartScreen = ({numQuestions, dispatch}) => {
   return (
     <div className="start"> 
         <h2>
             Welcome to the react quiz !!
         </h2>
         <h3>{numQuestions} questions to test your react mastery !</h3>
-        <button className="btn btn-ui">Let's start</button>
-      
+        <button className="btn btn-ui" onClick={() => dispatch({type: 'start'})}>Let's start</button>      
     </div>
   )
 };
